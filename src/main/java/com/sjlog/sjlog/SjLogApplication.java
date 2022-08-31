ad666288d9bb3f3df0eb0f7eea5e852a6df3c8d1
HanSungJun <hanry18@gmail.com>
2022-08-31 20:08:05 +0900

    Initial commit

---
+++ src/main/java/com/sjlog/sjlog/SjLogApplication.java
@@ -0,0 +1,13 @@
+package com.sjlog.sjlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SjLogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SjLogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sjlog/sjlog/SjLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sjlog.sjlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SjLogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
